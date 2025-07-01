ead5264d4d681b04f581a7ac2f7fc874bc38f959
Haruki Miyagi <haruki.miyagi11@gmail.com>
2025-07-01 17:07:41 +0900

    Fix DMG filename in URL (use URL-encoded space)

--- Casks/desktop-calendar.rb
+++ Casks/desktop-calendar.rb
@@ -2,7 +2,7 @@ cask "desktop-calendar" do
   version "1.0.0"
   sha256 "923515c4c83bf838bfcdd804e3398ee2d1fe936802239aafa7a09b2da9ce3f00"
 
-  url "https://github.com/hrk-m/desktop-calendar/releases/download/v#{version}/Desktop.Calendar-#{version}-arm64.dmg"
+  url "https://github.com/hrk-m/desktop-calendar/releases/download/v#{version}/Desktop%20Calendar-#{version}-arm64.dmg"
   name "Desktop Calendar"
   desc "Desktop calendar application for macOS with background operation"
   homepage "https://github.com/hrk-m/desktop-calendar"
